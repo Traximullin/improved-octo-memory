5e679a9a2ded19abae71da8e035377bcf98da227
rtr <rtr@vt2b.ru>
2023-01-31 12:21:30 +0500

    first commit

---
+++ presentation/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import styles from "./Button.module.scss"
+
+const Button:FC = () => {
+    return (
+        <button className={styles.button}>
+            BUTTON
+        </button>
+    )
+}
+
+export default Button
